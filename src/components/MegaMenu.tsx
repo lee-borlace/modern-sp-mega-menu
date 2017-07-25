4b96ce9cc5f17ef2a69613067b377208fca87ba7
Lee Borlace <lee.borlace@gmail.com>
2017-07-25 09:41:03 +0800

    Set up of mega menu TSX

--- src/components/MegaMenu.tsx
+++ src/components/MegaMenu.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Spinner, SpinnerType } from 'office-ui-fabric-react/lib/Spinner';
+
+import styles from './MegaMenu.module.scss';
 
 export interface IMegaMenuProps {
 }
@@ -10,7 +11,16 @@ export interface IMegaMenuState {
 export class MegaMenu extends React.Component<IMegaMenuProps, IMegaMenuState> {
 
     public render(): React.ReactElement<IMegaMenuProps> {
-        return <div>THIS IS THE REACT HEADER</div>;
+        return (
+            <div className={`ms-Grid ms-bgColor-themePrimary ms-fontColor-themeLighterAlt ms-fontSize-l ms-fadeIn100 ${styles.container}`}>
+                <div className="ms-Grid-row">
+                    <div className="ms-Grid-col ms-lg3 ms-sm12 ms-textAlignCenter">Departments</div>
+                    <div className="ms-Grid-col ms-lg3 ms-sm12 ms-textAlignCenter">Projects</div>
+                    <div className="ms-Grid-col ms-lg3 ms-sm12 ms-textAlignCenter">Resources</div>
+                    <div className="ms-Grid-col ms-lg3 ms-sm12 ms-textAlignCenter">About</div>
+                </div>
+            </div>
+        );
     }
 
 }
